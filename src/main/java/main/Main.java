5c3a6330799dd77b59aa4992403394091e9f948b
kv <kirill.v.kiselev@yandex.ru>
2018-08-30 19:06:26 +0300

    отработка автоматического инкремента версиии помника выполнена

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello Jenkins!");
         System.out.println("feature 1");
         System.out.println("feature 2");
+        System.out.println("Модный релиз посредством скрипта");
     }
 }
