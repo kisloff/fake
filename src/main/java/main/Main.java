3260a26f7a63301a46a471b329c330281cd84af2
kv <kirill.v.kiselev@yandex.ru>
2018-08-30 10:36:26 +0300

    feature 2

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,6 +3,7 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
-        System.out.println("feature1");
+        System.out.println("feature 1");
+        System.out.println("feature 2");
     }
 }
